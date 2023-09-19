b3c10ce346a8a1c03f11c9c87939411b2788eb94
kevinramos2 <keramosl@unal.edu.co>
2023-09-19 14:49:04 -0500

    Update Ave.java

--- src/test/java/zooAnimales/Ave.java
+++ src/test/java/zooAnimales/Ave.java
@@ -1,59 +1,53 @@
 package zooAnimales;
-import java.util.List;
 import java.util.ArrayList;
 
 public class Ave extends Animal{
 	/* ATRIBUTOS*/
-	private  static List<Ave> listado =new ArrayList<>();
+	private  static ArrayList<Ave> listado =new ArrayList<Ave>();
 	public static int halcones;
 	public static int aguilas;
 	private String colorPlumas;
+	private static int cantidadAves = 0;
 	
 	/*CONSTRUCTURES*/
 	public Ave(String nombre,int edad, String habitat,String genero, String colorPlumas) {
-		this.setNombre(nombre);
-		this.setEdad(edad);
-		this.setHabitat(habitat);
-		this.setGenero(genero);
-		this.colorPlumas=colorPlumas;
+		super(nombre,edad,habitat,genero);
+		this.colorPlumas = colorPlumas;
+		Ave.cantidadAves += 1;
 		Ave.listado.add(this);
 	}
-	
+	public Ave(String colorPlumas) {
+		this.colorPlumas = colorPlumas;
+		Ave.cantidadAves += 1;
+		Ave.listado.add(this);
+	}
 	public Ave() {
+		cantidadAves += 1;
 		Ave.listado.add(this);
 	}
 	
 	/*MÉTODOS*/
 	public String getColorPlumas() {
-		return colorPlumas;
+		return this.colorPlumas;
 	}
 
 	public void setColorPlumas(String colorPlumas) {
 		this.colorPlumas = colorPlumas;
 	}
-
-	public static List<Ave> getListado() {
-		return listado;
-	}
-
-	public static void setListado(List<Ave> listado) {
-		Ave.listado = listado;
-	}
-
-	public int cantidadAves() {
-		return listado.size();
+	public static int cantidadAves() {
+		return Ave.cantidadAves;
 	}
 	public String movimiento() {
 		return "volar";
 	}
-	
-	public static Animal crearHalcon(String nombre,int edad,String genero) {
-		Ave.halcones++;
-		return new Ave(nombre,edad,"montanas",genero,"cafe glorioso");
+	public static Ave crearHalcon(String nombre,int edad,String genero) {
+		Ave.halcones += 1;
+		Ave halcon = new Ave(nombre,edad,"montanas",genero,"cafe glorioso");
+		return halcon;
 	}	
-
-	public static Animal crearAguila(String nombre,int edad,String genero){
-		Ave.aguilas++;
-		return new Ave(nombre,edad,"montanas",genero,"blanco y amarillo");
+	public static Ave crearAguila(String nombre,int edad,String genero){
+		Ave.aguilas += 1;
+		Ave aguila = new Ave(nombre,edad,"montanas",genero,"blanco y amarillo");
+		return aguila;
 	}
 }

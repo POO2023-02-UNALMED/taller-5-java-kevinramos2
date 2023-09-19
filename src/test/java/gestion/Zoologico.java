81317ffa18090fb1a8efd953545f2d1cb6d57ac3
kevinramos2 <keramosl@unal.edu.co>
2023-09-19 14:14:37 -0500

    Update Zoologico.java

--- src/test/java/gestion/Zoologico.java
+++ src/test/java/gestion/Zoologico.java
@@ -1,12 +1,11 @@
 package gestion;
-import java.util.List;
 import java.util.ArrayList;
 
 public class Zoologico {
 	/* ATRIBUTOS*/
 	private String nombre;
 	private String ubicacion;
-	private  List<Zona> zonaList = new ArrayList<Zona>();
+	private ArrayList<Zona> zonas = new ArrayList<Zona>();
 	
 	/*CONSTRUCTURES*/
 	public Zoologico (String nombre, String ubicacion) {
@@ -30,20 +29,17 @@ public class Zoologico {
 	public void setUbicacion(String ubicacion) {
 		this.ubicacion = ubicacion;
 	}
-	public List<Zona> getZona() {
-		return zonaList;
-	}
-	public void setZona(List<Zona> zonaList) {
-		this.zonaList = zonaList;
+	public ArrayList<Zona> getZona() {
+		return zonas;
 	}
 	
 	public void agregarZona(Zona zona) {
-		this.zonaList.add(zona);
+		zonas.add(zona);
 	}
 	public int cantidadTotalAnimales() {
 		int totalAnimales = 0;
-		for(Zona zona: zonaList) {
-			totalAnimales+=zona.cantidadAnimales();
+		for(Zona zona: zonas) {
+			totalAnimales += zona.cantidadAnimales();
 		}
 		return totalAnimales;
 	}

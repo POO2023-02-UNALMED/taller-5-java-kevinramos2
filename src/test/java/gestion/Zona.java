8b252a8c1c1ff0c30a4571e6e60a373cfc760135
kevinramos2 <keramosl@unal.edu.co>
2023-09-19 15:41:04 -0500

    Update Zona.java

--- src/test/java/gestion/Zona.java
+++ src/test/java/gestion/Zona.java
@@ -1,50 +1,51 @@
 package gestion;
 import java.util.ArrayList;
+
 import zooAnimales.Animal;
 
 public class Zona {
 	//ATRIBUTOS
-	private String nombre;
-	private Zoologico[] zoo = new Zoologico[1];
-	private  ArrayList<Animal> animales = new ArrayList<Animal>();
-	
-	//CONSTRUCTURES
-	public Zona() {
-		
-	}
-	public Zona(String nombre, Zoologico zoo) {
-		this.nombre = nombre;
-		this.zoo[0] = zoo;
-	}
+    private String nombre;
+    private Zoologico[] zoo = new Zoologico[1];
+    private ArrayList<Animal> animales = new ArrayList<Animal>();
 
-	
-	//METODOS
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public Zoologico getZoo() {
-		return zoo[0];
-	}
-	public void setZoo(Zoologico[] zoo) {
-		this.zoo = zoo;
-	}
-	public ArrayList<Animal> getAnimales(){
-		return animales;
-	}
-
-	public void agregarAnimales(Animal animal) {
-		animales.add(animal);
-	}
-	public int cantidadAnimales() {
-		int total = 0;
-		for (Animal animal : animales) {
-			if (animal != null) {
-				total += 1;
-			}
-		}
-		return total;
-	}
+    //CONSTRUCTORES
+    public Zona(){
+    	
+    }
+    public Zona(String nombre, Zoologico zoo){
+        this.nombre = nombre;
+        this.zoo[0] = zoo;
+    }
+    //METODOS
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Zoologico getZoo() {
+        return zoo[0];
+    }
+    public void setZoo(Zoologico[] zoo) {
+        this.zoo = zoo;
+    }
+    public ArrayList<Animal> getAnimales() {
+        return animales;
+    }
+    public void agregarAnimales(Animal animal){
+        animales.add(animal);
+    }
+    public Zoologico getZoologico(){
+        return zoo[0];
+    }
+    public int cantidadAnimales(){
+        int cantidad = 0;
+        for (Animal animal : animales) {
+            if(animal != null){
+                cantidad += 1;
+            }
+        }
+        return cantidad;
+    }
 }
